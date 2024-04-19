862d14e506e3e6418932b1d32212fda8315120b9
Alex <143654953+Cityships@users.noreply.github.com>
2024-04-18 22:40:48 -0400

    scene 3 example with polygonal flooring
    dev notes and issues: Copying and pasting tile maps seems to cause issues. It deletes the tilemap from the copy source. Still solving how this problem.

--- assets/scripts/drawcollisionpolygon.gd
+++ assets/scripts/drawcollisionpolygon.gd
@@ -6,8 +6,8 @@ var polygon_visual
 func _ready():
 	vertice_vector = polygon
 	polygon_visual = get_child(0) as Polygon2D
-	for vertex in vertice_vector:
-		print("Vertex: ", vertex)
-	print("child: ", polygon_visual)
+	#for vertex in vertice_vector:
+		#print("Vertex: ", vertex)
+	#print("child: ", polygon_visual)
 	polygon_visual.polygon = vertice_vector
 	

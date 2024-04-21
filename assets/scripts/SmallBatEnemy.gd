3ea4e7dc5797021e485d08ff405da3c6dc648b3b
Alex <143654953+Cityships@users.noreply.github.com>
2024-04-21 01:15:11 -0400

    ice cube bats try to go above the fire
    todo: ice cube bats drop ice
    summoner bats summon other bats.

--- assets/scripts/SmallBatEnemy.gd
+++ assets/scripts/SmallBatEnemy.gd
@@ -18,7 +18,8 @@ var channeling = false
 var summon_cooldown = 13
 @export var max_summon_cooldown = 10
 @onready var hit_box = $Hitbox
-
+@onready var ice_cube = $icecube
+@onready var offset = Vector2(0, -200)
 
 func channel_summon():
 	anim.speed_scale = 1
@@ -36,6 +37,9 @@ func _defeated():
 	self.queue_free()
 
 func _ready():
+	if !is_summoner:
+		ice_cube.visible = true
+		target_position = get_parent().get_node("campfire").position + offset
 	timer.timeout.connect(_on_timer_timeout)
 	#connect the signal
 	
@@ -44,15 +48,16 @@ func _physics_process(delta):
 	if !anim.is_playing():
 		anim.play("idlemove")
 	var player = get_parent().get_node("player")
-	target_position = player.position
+	if is_summoner:
+		target_position = player.position
 	var direction = (target_position - position).normalized()
 	
 	if channeling:
 		anim.speed_scale += channel_anim_speed_ramp * delta
 	if summon_cooldown >= 0:
 		summon_cooldown -= delta
-	if player.holding_item && position.distance_to(target_position) < scare_range:
-		anim.play("flyaway")
+	#if player.holding_item && position.distance_to(target_position) < scare_range:
+	#	anim.play("flyaway")
 	if hit_box.is_colliding():
 		var object = hit_box.get_collider(0) as RigidBody2D
 		if object != null && !(object.linear_velocity.is_zero_approx()):
@@ -64,6 +69,9 @@ func _physics_process(delta):
 			channel_summon()
 	elif is_summoner:
 		direction = Vector2()
+
+	
+	
 	
 	if direction.x < 0 && sprite.flip_h:
 		sprite.flip_h = false
